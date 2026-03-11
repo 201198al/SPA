bf7092d8818e4fac9d3c454917e35398fdcc278d
201198al <201198al@gmail.com>
2026-03-11 16:57:04 +1000

    Логин

---
+++ login.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once 'functions.php';
+
+if (getCurrentUser() !== null) {
+    header("Location: index.php");
+    exit;
+}
+
+$error = "";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+
+    if (checkPassword($login, $password)) {
+
+        $_SESSION['login'] = $login;
+        $_SESSION['login_time'] = time();
+
+        header("Location: index.php");
+        exit;
+
+    } else {
+
+        $error = "Неверный логин или пароль";
+
+    }
+
+}
+
+?>
+
+<h2>Вход</h2>
+
+<form method="POST">
+
+<p>Логин</p>
+<input name="login">
+
+<p>Пароль</p>
+<input type="password" name="password">
+
+<br><br>
+
+<button>Войти</button>
+
+</form>
+
+<p style="color:red"><?php echo $error; ?></p>
+
+<a href="register.php">Регистрация</a>
