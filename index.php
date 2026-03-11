b84f6813adfa4e011caa752ea08413f085db2c6a
201198al <201198al@gmail.com>
2026-03-11 17:56:24 +1000

    День рождения

--- index.php
+++ index.php
@@ -61,3 +61,49 @@ if ($user && isset($_SESSION['login_time'])) {
 }
 
 ?>
+
+<?php
+
+if ($user) {
+
+    $file = file("users.txt");
+
+    foreach ($file as $line) {
+
+        list($login,$hash,$birthday) = explode(":", trim($line));
+
+        if ($login === $user) {
+
+            $today = new DateTime();
+            $bday = new DateTime($birthday);
+
+            $bday->setDate(
+                $today->format("Y"),
+                $bday->format("m"),
+                $bday->format("d")
+            );
+
+            if ($today > $bday) {
+                $bday->modify("+1 year");
+            }
+
+            $diff = $today->diff($bday)->days;
+
+            if ($diff == 0) {
+
+                echo "<h2>Поздравляем Вас С ДНЕМ РОЖДЕНИЯ!</h2>";
+                echo "<p>Скидка 20% на все услуги!</p>";
+
+            } else {
+
+                echo "<p>До вашего дня рождения осталось $diff дней</p>";
+
+            }
+
+        }
+
+    }
+
+}
+
+?>
